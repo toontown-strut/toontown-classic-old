3839965166d008f0ed1b81e78bb2cc4553bf42f4
Samuel Lord <sam@sllord.info>
2022-08-01 11:17:44 -0500

    Add files via upload

--- win32/start_game.bat
+++ win32/start_game.bat
@@ -6,6 +6,7 @@ rem Read the contents of PPYTHON_PATH into %PPYTHON_PATH%:
 set /P PPYTHON_PATH=<PPYTHON_PATH
 
 set /P LOGIN_TOKEN="Secret token: "
+set /P GAME_SERVER="Game server: "
 :A
 %PPYTHON_PATH% -m toontown.launcher.QuickStartLauncher
 GOTO A
